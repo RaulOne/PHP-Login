074452c31e9f29d483407b613a43e446e633d136
RaulOne <rulo@hushmail.com>
2015-01-22 01:35:11 -0300

    Final version

--- includes/newuser.php
+++ includes/newuser.php
@@ -16,27 +16,56 @@ class Newuser{
 	
 	private function doUser(){
 		
-
+		if(empty($_POST['user'])){
+			$this->messages[]='Empty user. Try again.';
+		}
+		else if(empty($_POST['password'])){
+			$this->messages[]='Empty password. Try again.';
+		}
+		else if(empty($_POST['email'])){
+			$this->messages[]='Empty email. Try again.';
+		}
+		else if(empty($_POST['password_repeat'])){
+			$this->messages[]='Empty password repeat. Try again.';
+		}
+		else if($_POST['password']!=$_POST['password_repeat']){
+			$this->messages[]='password and password repeat does not match. Try again.';
+		}
+		else if( strlen($_POST['password'])<6 || strlen($_POST['password'])>50 ){
+			$this->messages[]='password cant be less than 6 characters or more than 50. Try again.';
+		}
+		else if( strlen($_POST['user'])<6 || strlen($_POST['user'])>50){
+			$this->messages[]='user cant be less than 6 characters or more than 50.';
+		}
+		else if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+			$this->messages[]='Email is not valid. Try again.';
+		}
+		else if(!preg_match('/^[a-zA-Z0-9._-]{6,50}$/',$_POST['user'])){
+			$this->messages[]='User must have between 6 and 50 alphanumeric charachers and _,.,-. Try again.';
+		}
+		else{
 		$this->db=new mysqli(SERVER,USER,PASSWORD,DATABASE);
 
 		if(!$this->db->connect_errno){
 
-			$user=$this->db->real_escape_string($_POST['user']);
-			$email=$this->db->real_escape_string($_POST['email']);
+			$user=$this->db->real_escape_string(strip_tags($_POST['user'],ENT_QUOTES));
+			$email=$this->db->real_escape_string(strip_tags($_POST['email'],ENT_QUOTES));
 			$password=$_POST['password'];
 			$password_hash=password_hash($password,PASSWORD_DEFAULT);
 			$privileges=1;
 
-			$sql="SELECT user, email, password, privileges FROM usuarios WHERE user='$user';";
+			$sql="SELECT * FROM usuarios WHERE user='$user' OR email='$email';";
+			
 			$resource=$this->db->query($sql);
 
 			if($resource->num_rows === 1){
 
-				$this->warnings[] = 'User already exists. Try another user.';
+				$this->warnings[] = 'User or email already exists. Try another user and email.';
 			
 			}else{
 			
 			$sql="INSERT INTO usuarios values('$user','$password_hash','$email','$privileges');";
+			
 			$result=$this->db->query($sql);
 
 			if($result){
@@ -45,7 +74,7 @@ class Newuser{
 			
 			}else{
 			
-				$this->warnings[] = 'There was some problem creating new user. Try again';
+				$this->warnings[] = 'There has been some problem creating new user. Try again';
 			
 			}
 			
@@ -56,7 +85,7 @@ class Newuser{
 			$this->warnings[] = "Database not working.";
 		}
 
-		
+		}
 
 	}
 }
--- views/register.php
+++ views/register.php
@@ -17,14 +17,14 @@ if(isset($newuser)){
 
 <form action="register.php" method="post">
 	
-	<label for="user_name">User:</label>
-	<input type="text" name="user" id="user_name" required>
-	<label for="user_email">Email:</label>
-	<input type="email" name="email" id="user_email" required>
-	<label for="user_password">Password</label>
-	<input type="password" name="password" id="user_password" required>
+	<label for="user_name">User - Between 6 and 50 alphanumeric characters</label>
+	<input type="text" name="user" id="user_name" pattern="[a-zA-z0-9_.-]{6,50}" required><br />
+	<label for="user_email">Email</label>
+	<input type="email" name="email" id="user_email" required><br />
+	<label for="user_password">Password - Between 6 and 50 characters</label>
+	<input type="password" name="password" id="user_password" pattern="[^>]*{6,50}" required><br />
 	<label for="user_password_repeat">Repeat password:</label>
-	<input type="password" name="password_repeat" id="user_password_repeat" required>
+	<input type="password" name="password_repeat" id="user_password_repeat" pattern="[^>]*{6,50}" required><br />
 	<input type="submit" name="register" value="register">
 
 </form>
